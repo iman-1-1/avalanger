5b9bc3a0a2da0195a917fbf66f71f844ebcfb924
Iman <akramima@students.zhaw.ch>
2026-02-27 14:05:19 +0100

    added avalange controller

--- src/main/java/ch/zhaw/avalanger/controller/AvalangeController.java
+++ src/main/java/ch/zhaw/avalanger/controller/AvalangeController.java
@@ -1,33 +1,17 @@
 package ch.zhaw.avalanger.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import ch.zhaw.avalanger.model.Avalange;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("/api/avalange")
 public class AvalangeController {
 
-    @GetMapping(value = {"", "/{country}"})
-    public String getAllAvalanges(@PathVariable(required = false) String country, 
-                                  @RequestParam(required = false) String state) {
-        if (country == null || country.isEmpty()) {
-            return "No avalanges found";
-        }
-        return "No avalanges found for country: " + country + ", state: " + state;
+    @GetMapping("")
+    public String getAllAvalange() {
+        return "No avalange found.";
     }
-
-    @PostMapping("")
-    public String postMethodName(@RequestBody Avalange avalange) {
-        
-        return "Avalange received: " + avalange.getCountry() + ", " + avalange.getState() + ", " + avalange.getDescription();
-    }
+    
+    
 }
